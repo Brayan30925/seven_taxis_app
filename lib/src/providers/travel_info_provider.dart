e7eb59f3c886488592eea5aaa0c4e5d5de98fe6f
eduvan martinez <duvan20902@gmail.com>
2024-09-13 07:23:23 -0500

    Merge branch 'version_prueba' into master video 65

---
+++ lib/src/providers/travel_info_provider.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:uber_clone_flutter_udemy/src/models/travel_info.dart';
+
+class TravelInfoProvider {
+
+  CollectionReference _ref;
+
+  TravelInfoProvider() {
+    _ref = FirebaseFirestore.instance.collection('TravelInfo');
+  }
+
+  Future<void> create(TravelInfo travelInfo) {
+    String errorMessage;
+
+    try {
+      return _ref.doc(travelInfo.id).set(travelInfo.toJson());
+    } catch(error) {
+      errorMessage = error.code;
+    }
+
+    if (errorMessage != null) {
+      return Future.error(errorMessage);
+    }
+
+  }
+
+}
